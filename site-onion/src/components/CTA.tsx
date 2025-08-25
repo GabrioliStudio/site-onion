294131275cc17b1413a89e3c98d972444594bbeb
gabriel <gabrioli.dev@gmail.com>
2025-08-24 22:37:15 -0300

    updating hero and cta

--- site-onion/src/components/CTA.tsx
+++ site-onion/src/components/CTA.tsx
@@ -1,21 +1,19 @@
+import { useLanguage } from "@/contexts/LanguageContext";
 import { Button } from "@/components/ui/button";
 
 const CTA = () => {
   return (
-    <section className="py-20 bg-gradient-hero">
-      <div className="container mx-auto px-6 text-center">
-        <div className="max-w-4xl mx-auto">
-          <h2 className="text-4xl md:text-6xl font-bold text-foreground mb-6">
-            Grandes marcas não são criadas por acaso.
+    <section className="px-[40px] bg-white h-screen grid items-center">
+      <div className="w-full py-[40px] px-[40px] border border-border text-center bg-white gap-[40px] rounded-lg">
+
+        <div className="py-[40px] px-[40px] bg-[url('/lovable-uploads/background.png')] bg-cover bg-center bg-no-repeat gap-[40px] flex flex-wrap rounded-lg">
+        <img src="/lovable-uploads/cta-logo.svg" alt="" />
+          <h2 className="text-3xl md:text-4xl font-bold text-foreground leading-[90%] whitespace-pre-line">
+            {"Grandes marcas não construídas por acaso.\nVamos construir a sua?"}
           </h2>
-          
-          <p className="text-xl text-muted-foreground mb-8">
-            Vamos construir a sua?
-          </p>
-          
           <Button 
             size="lg" 
-            className="bg-secondary text-secondary-foreground hover:bg-secondary/90 px-8 py-4 text-lg font-semibold rounded-full transition-all duration-300 hover:shadow-glow"
+            className="bg-secondary text-secondary-foreground px-8 py-4 text-lg font-semibold rounded-full transition-all duration-300 hover:shadow-glow"
           >
             Começar projeto
           </Button>
--- site-onion/src/components/HeroSection.tsx
+++ site-onion/src/components/HeroSection.tsx
@@ -4,32 +4,23 @@ const HeroSection = () => {
   const { t } = useLanguage();
 
   return (
-    <section className="min-h-screen bg-black-hero flex items-center relative overflow-hidden">
-      <div className="w-full h-screen px-[40px] flex flex-col gap-20 pb-[50px] justify-end">
+    <section className="min-h-screen bg-[url('/lovable-uploads/background.png')] bg-cover bg-center bg-no-repeat flex items-center relative overflow-hidden">
+      <div className="w-full h-screen px-[40px] flex flex-col py-[40px] justify-end">
 
-
-        <div className="flex flex-col itens-end relative">
-
-          <div className="hero-badge-bg bg-white absolute top-[12%]">
-            <div className="hero-badge"></div>
-          </div>
-
-          <div className="w-full">
-            <h1 className="hero-title text-foreground leading-tight whitespace-pre-line">
+          <div className="w-full mb-[80px]">
+            <h1 className="text-[7.5vw] sm:text-[8w] md:text-[9vw] xl:text-[10.2vw] leading-[90%] whitespace-pre-line">
               {t('hero.title')}
             </h1>
           </div>
 
-        </div>
-
         <div className="flex flex-col gap-[20px]">
-          <small className="text-muted-foreground whitespace-pre-line">
+          <small className="text-muted-foreground whitespace-pre-line text-[10px]">
             {t('hero.copy')}
           </small>
 
           <div className="h-[1px] bg-white w-full"></div>
 
-          <p className="text-sm, md:text text-muted-foreground whitespace-pre">
+          <p className="text-sm, md:text text-muted-foreground whitespace-pre leading-[102%] text-[16px]">
             {t('hero.subtitle')}
           </p>
         </div>
